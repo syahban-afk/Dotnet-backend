42c6534bcb4caba22e220b89da97128953dc63b2
syahban-afk <syahbansyahputrati@gmail.com>
2025-08-13 10:32:10 +0700

    memperbarui CRUD

--- obj/Debug/net9.0/MyProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d61f0b3578a0a9c6d70e4ae5fcc38e10032d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426e45b593b82be1e36c985543e14de18d79cb89")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
